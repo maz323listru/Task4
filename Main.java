b6562a997ce4c30fdf6c0c89ecf519213d4dd336
Benevolenski Aleksey <53492441+maz323listru@users.noreply.github.com>
2019-09-09 00:40:00 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,51 +1,121 @@
 package com.company;
 
 public class Main {
-    public static class Time {
-        int second;
-        int minute;
-        int hour;
+    public static class Bankomat {
+        int banknote_20;
+        int banknote_50;
+        int banknote_100;
 
-        Time(int h, int m, int s) {
-            hour = h;
-            minute = m;
-            second = s;
-        }
 
-        Time(int sec) {
-            second = sec;
+        void addMoney(int add20, int add50, int add100) {
+            banknote_20 += add20;
+            banknote_50 += add50;
+            banknote_100 += add100;
+            System.out.println("В банкомате следующее количество купюр: \nбанкнот 20 рублей: " + banknote_20 + "\nбанкнот 50 рублей: " + banknote_50 + "\nбанкнот 100 рублей:" + banknote_100);
         }
 
-        int fullSecond() {
-            return (hour * 60 + minute) * 60 + second;
+        Bankomat(int ban_20, int ban_50, int ban_100) {
+            banknote_20 = ban_20;
+            banknote_50 = ban_50;
+            banknote_100 = ban_100;
         }
-        void returnFullTime(){
-            int H;
-            int M;
-            int S;
-            H=this.second/3600;
-            M=(this.second%3600)/60;
-            S=(this.second%3600)%60;
-            System.out.println("Время Ч:М:С "+H+" "+M+" "+S);
+
+        int sumGivingOutMoney() {
+            return banknote_20 * 20 + banknote_50 * 50 + banknote_100 * 100;
         }
-        void printTime(){
-        System.out.println("Время ЧАС:МИН:СЕК "+hour+":"+minute+":"+second);
+
+        boolean checkGivingOut(int x) {
+            int sum = sumGivingOutMoney();
+            if (sum >= x) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        void calculation_1(int sumCash){
+            sumCash = sumCash - 50;
+                int x100 = sumCash / 100;
+                int x50=0;
+                int x20 = ((sumCash - x100 * 100) / 20);
+                if (x100 <= banknote_100) {
+                    System.out.println("Количество банкнот номиналом 100 рублей: " + x100);
+                }
+                else {
+                    x50=sumCash/50;
+                    x20=(sumCash-x50*50)/20;
+                }
+                if (x50 <= banknote_50) {
+                    System.out.println("Количество банкнот номиналом 50 рублей: " + (x50+1));
+                }
+                if (x20 <= banknote_20) {
+                    System.out.println("Количество банкот номиналом 20 рублей: " + x20);
+                }
+        }
+
+
+        void calculation(int sumCash) {
+            int x100 = sumCash / 100;
+            int x50 = (sumCash - x100 * 100) / 50;
+            int conX50=(sumCash - x100 * 100) - (x50 * 50);
+            int x20 = conX50 / 20;
+            int check=conX50 % 20;
+            boolean val = checkGivingOut(sumCash);
+            if (val && sumCash/10%2!=0 && sumCash%10==0 && sumCash!=30){
+                calculation_1(sumCash);
+            }
+            else if (val && check==10 ){
+                x50=0;
+                if (x100 <= banknote_100) {
+                    System.out.println("Количество банкнот номиналом 100 рублей: " + x100);
+                    x20=(sumCash-x100*100)/20;
+                    System.out.println("Количество банкнот номиналом 50 рублей: " + x50);
+                    System.out.println("Количество банкот номиналом 20 рублей: " + x20);
+                }
+                else {
+                    if (x50<= banknote_50){
+                        System.out.println("Количество банкнот номиналом 100 рублей: 0");
+                        x50=x100*2;
+                        x20=(sumCash-x50*50)/20;
+                        System.out.println("Количество банкнот номиналом 50 рублей: " + x50);
+                        System.out.println("Количество банкот номиналом 20 рублей: " + x20);
+                    }
+                    else {
+                        x20=sumCash/20;
+                        System.out.println("Количество банкот номиналом 20 рублей: "+x20);
+                    }
+                }
+
+            }
+            else if (val && (sumCash % 20 == 0 || sumCash % 50 == 0 || sumCash % 100 == 0 )) {
+
+                if (x100 <= banknote_100) {
+                    System.out.println("Количество банкнот номиналом 100 рублей: " + x100);
+                }
+                else {
+                    x50=sumCash/50;
+                    x20=(sumCash-x50*50)/20;
+                }
+                if (x50 <= banknote_50) {
+                    System.out.println("Количество банкнот номиналом 50 рублей: " + x50);
+                }
+                else {
+                    x20=sumCash/20;
+                }
+                if (x20 <= banknote_20) {
+                    System.out.println("Количество банкот номиналом 20 рублей: " + x20);
+                }
+            } else {
+                System.out.println("Данную операцию невозможно выполнить");
+            }
         }
     }
+
 
     public static void main(String[] args) {
-        int s;
-            Time time1 = new Time(5,56,45);
-            Time time2 = new Time(24560);
-            s=time1.fullSecond();
-
-      time1.printTime();
-      System.out.println("Общее количество секунд: "+s);
-      time2.returnFullTime();
-
-        }
+        Bankomat gom = new Bankomat(200, 400, 500);
+        gom.calculation(280);
+        gom.calculation(185);
+        gom.addMoney(56, 89, 47);
 
     }
-// write your code here
-
-
+}
